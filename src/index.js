e7fc0f4193ad173617d164898128c9eb4ca4c3ce
WadimSh <wadim11@yandex.ru>
2022-10-06 13:22:19 +0300

    fix: сделана логика слайдера

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import './index.css';
+
+let slideIndex = 1;
+const nextSlide = document.querySelector('.comment__next-button');
+const previousSlide = document.querySelector('.comment__previous-button');
+const submitButton = document.querySelector('.partners__submit');
+
+const showSlides = (n) => {
+  let slides = document.getElementsByClassName("comment__item");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (let slide of slides) {
+    slide.style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "flex";
+}
+
+nextSlide.addEventListener('click', () => {
+	showSlides(slideIndex += 1);
+});
+
+previousSlide.addEventListener('click', () => {
+  showSlides(slideIndex -= 1);
+});
+
+submitButton.addEventListener('click', () => {
+  evt.preventDefault();
+})
+
+showSlides(slideIndex);
